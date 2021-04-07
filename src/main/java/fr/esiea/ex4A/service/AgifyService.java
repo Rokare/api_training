b778c908a56fafd9ddb1a98979238ec999bf9336
rokare <maxime.abensur@gmail.com>
2021-04-07 21:52:30 +0200

    fix missing final and too long method

--- src/main/java/fr/esiea/ex4A/service/AgifyService.java
+++ src/main/java/fr/esiea/ex4A/service/AgifyService.java
@@ -3,7 +3,6 @@ package fr.esiea.ex4A.service;
 import fr.esiea.ex4A.agify.AgifyClient;
 import fr.esiea.ex4A.agify.AgifyResponse;
 import org.springframework.stereotype.Service;
-import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
@@ -21,17 +20,13 @@ public class AgifyService {
     }
     public int getAge(String userName, String countryId) {
         int age = 0;
-        Call<AgifyResponse> call = agifyClient.getResponce(userName, countryId);
         try {
-            Response<AgifyResponse> response = call.execute();
+            Response<AgifyResponse> response = agifyClient.getResponce(userName, countryId).execute();
             if (response.isSuccessful()) {
-                AgifyResponse result = response.body();
-                responseService.addResponse(result);
-                age = result.getAge();
+                responseService.addResponse(response.body());
+                age = response.body().getAge();
             }
-        } catch (IOException ex){
-            ex.printStackTrace();
-        }
+        } catch (IOException ex){ ex.printStackTrace(); }
         return age;
     }
 }
